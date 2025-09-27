77032b2c42d2d064694e4c7fe746874a00157b63
Tamás Görgics <gorgics.t@gmail.com>
2025-09-27 20:22:10 +0200

    logger interface

--- pkg/logx/logx.go
+++ pkg/logx/logx.go
@@ -5,8 +5,11 @@ import (
 	"log/slog"
 )
 
-type logger struct {
-	l *slog.Logger
+type Logger interface {
+	Debug func(context.Context, string, ...any)
+	Info func(context.Context, string, ...any)
+	Warn func(context.Context, string, ...any)
+	Error func(context.Context, string, ...any)
 }
 
 var (
